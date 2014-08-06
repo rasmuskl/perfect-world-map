6d7f15c29fd2e06ca09495d42acdc46b10be09b2
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2014-08-06 19:23:50 +0200

    added unzip.

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,4 +1,9 @@
 mkdir data
+
 wget --no-clobber --output-document=data/10m_cultural.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zip
 wget --no-clobber --output-document=data/10m_physical.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m_physical.zip
 wget --no-clobber --output-document=data/NE2_HR_LC_SR_W_DR.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/raster/NE2_HR_LC_SR_W.zip
+
+unzip -u -d data/cultural data/10m_cultural.zip
+unzip -u -d data/physical data/10m_physical.zip
+unzip -u -d data/raster data/NE2_HR_LC_SR_W_DR.zip
