85acdac0ba4b44e6a6b128b671cda64fe13c27e0
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2014-08-06 20:16:35 +0200

    added more info.

--- fetch-data.sh
+++ fetch-data.sh
@@ -7,3 +7,5 @@ wget --no-clobber --output-document=data/NE2_HR_LC_SR_W_DR.zip http://www.natura
 unzip -u -d data/cultural data/10m_cultural.zip
 unzip -u -d data/physical data/10m_physical.zip
 unzip -u -d data/raster data/NE2_HR_LC_SR_W_DR.zip
+
+/Applications/Postgres.app/Contents/Versions/9.3/bin/shp2pgsql -s 4326 -I -W LATIN1 data/cultural/10m_cultural/ne_10m_admin_0_countries.shp ne_10m_admin_0_countries | /Applications/Postgres.app/Contents/Versions/9.3/bin/psql -d map
