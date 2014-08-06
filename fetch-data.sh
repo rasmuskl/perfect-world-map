3ae566e7e6833979ab3d2fc4f0526c260aea42f2
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2014-08-06 19:10:56 +0200

    added fetch data script for natural earth data.

---
+++ fetch-data.sh
@@ -0,0 +1,4 @@
+mkdir data
+wget --no-clobber --output-document=data/10m_cultural.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zip
+wget --no-clobber --output-document=data/10m_physical.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m_physical.zip
+wget --no-clobber --output-document=data/NE2_HR_LC_SR_W_DR.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/raster/NE2_HR_LC_SR_W.zip
